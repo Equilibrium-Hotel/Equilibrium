c0bd5f267e5b5e95d198e7102fabe11f831c0e3a
Pavel Ilchev <pavel_ilchev@abv.bg>
2016-12-03 17:19:55 +0200

    reviews paging

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/App';
 import './styles/index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import {browserHistory, Router, Route, IndexRoute} from 'react-router'
-import HomeView from './views/Home/HomeView'
-import ReviewView from './views/Review/ReviewView'
-import Reviews from './views/Review/Reviews'
-import LeaveReview from './views/LeaveReview/LeaveReview'
-import AboutView from './views/About/AboutView'
-import LoginView from './views/Login/LoginView'
-import RegisterView from './views/Register/RegisterView'
-import BookingView from './views/Booking/BookingView'
-import ContactsView from './views/Contacts/ContactsView'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {browserHistory, Router, Route, IndexRoute} from 'react-router';
+import HomeView from './views/Home/HomeView';
+import ReviewView from './views/Review/ReviewView';
+import LeaveReview from './views/LeaveReview/LeaveReview';
+import AboutView from './views/About/AboutView';
+import LoginView from './views/Login/LoginView';
+import RegisterView from './views/Register/RegisterView';
+import BookingView from './views/Booking/BookingView';
+import ContactsView from './views/Contacts/ContactsView';
 
 ReactDOM.render(
     <Router history={browserHistory}>
--- src/views/Review/ReviewView.js
+++ src/views/Review/ReviewView.js
@@ -2,30 +2,59 @@ import React from 'react';
 import {loadReviews} from '../../models/ReviewModel';
 import Reviews from './Reviews';
 
+import Pagination from "../../utils/Pagination";
+
 import '../../styles/Reviews.css'
 
 export default class ReviewView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            reviews: []
+            reviews: [],
+            currentPageReviews: [],
+            reviewsPerPage : 5,
+            activePage: 1
         };
 
         this.onLoadSuccess = this.onLoadSuccess.bind(this);
+        this.handlePageChange = this.handlePageChange.bind(this);
+        this.setCurrentPageReviews = this.setCurrentPageReviews.bind(this);
     }
 
     onLoadSuccess(response) {
-         this.setState({reviews: response})
+        this.setState({reviews: response.reverse()})
+        this.setCurrentPageReviews();
     }
 
     componentDidMount() {
         loadReviews(this.onLoadSuccess);
     }
 
+    setCurrentPageReviews() {
+        let currentPage = this.state.activePage - 1;
+        let countPerPage = this.state.reviewsPerPage;
+        let start = currentPage * countPerPage;
+
+        let reviews = this.state.reviews.slice(start, start + countPerPage);
+
+        this.setState({currentPageReviews: reviews});
+    }
+
+    handlePageChange(pageNumber) {
+        this.setState({activePage: pageNumber}, this.setCurrentPageReviews);
+    }
+
     render() {
         return (
             <div className="reviews-wrapper">
-               <Reviews reviews={this.state.reviews} />
+                <Reviews reviews={this.state.currentPageReviews}/>
+                <Pagination
+                    activePage={this.state.activePage}
+                    itemsCountPerPage={this.state.reviewsPerPage}
+                    totalItemsCount={this.state.reviews.length}
+                    pageRangeDisplayed={5}
+                    onChange={this.handlePageChange}
+                />
             </div>
         )
     }
